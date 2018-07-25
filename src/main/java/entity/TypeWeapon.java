d0add45bf70169880afd5c48c23d9d84771377ef
Montego <bar_d91@mail.ru>
2018-07-25 20:57:21 +0300

    1.5.2

---
+++ src/main/java/entity/TypeWeapon.java
@@ -0,0 +1,5 @@
+package entity;
+
+public enum TypeWeapon {
+    SWORD, SPEAR, MACE
+}
--- src/main/java/entity/Weapon.java
+++ src/main/java/entity/Weapon.java
@@ -1,8 +1,6 @@
 package entity;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table(name = "weapons")
@@ -13,4 +11,7 @@ public class Weapon extends AbstractEntity {
 
     @Column(nullable = false)
     private int damage;
+
+    @Enumerated(EnumType.STRING)
+    private TypeWeapon typeWeapon;
 }
