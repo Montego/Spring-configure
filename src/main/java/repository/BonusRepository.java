87acd3353c4527238a2b8ebf7687d75462b0af01
Montego <bar_d91@mail.ru>
2018-07-26 20:56:49 +0300

    1.5.3.2

---
+++ src/main/java/repository/BonusRepository.java
@@ -0,0 +1,9 @@
+package repository;
+
+import entity.Bonus;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BonusRepository extends JpaRepository<Bonus, Long> {
+
+
+}
---
+++ src/main/java/repository/RaceRepository.java
@@ -0,0 +1,7 @@
+package repository;
+
+import entity.Race;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RaceRepository extends JpaRepository<Race,Long> {
+}
