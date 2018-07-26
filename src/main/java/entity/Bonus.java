ad01c1481eee563f0120bd9c37e900999f8cafec
Montego <bar_d91@mail.ru>
2018-07-26 20:51:50 +0300

    1.5.3.2

---
+++ src/main/java/entity/Bonus.java
@@ -0,0 +1,14 @@
+package entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "bonuses")
+public class Bonus extends AbstractEntity{
+    @Column
+    private int attackBonus;
+    @Column
+    private int defenseBonus;
+}
--- src/main/java/entity/Race.java
+++ src/main/java/entity/Race.java
@@ -1,10 +1,16 @@
 package entity;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
+import java.util.List;
 
 @Entity
 @Table(name = "races")
 public class Race extends AbstractEntity{
-
+    @Column(unique = true,nullable = false)
+    private String name;
+    @OneToMany
+    private List<Bonus> bonus;                                                                      //TODO уточнить список ли?
 }
