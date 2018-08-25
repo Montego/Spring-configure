d686344f2dd24ce489c52a373e48995dec513bf9
Montego <bar_d91@mail.ru>
2018-08-25 14:55:38 +0300

    security config 2.0

--- src/main/java/security/SecurityConfig.java
+++ src/main/java/security/SecurityConfig.java
@@ -1,21 +1,85 @@
 package security;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.security.web.access.AccessDeniedHandlerImpl;
+
+import java.io.IOException;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private AccessDeniedHandler accessDeniedHandler;  //TODO разобрать
 
     private UserDetailsService userDetailsService;
     private IAuthenticationService authenticationService;
 
     @Override
-    protected void configure(HttpSecurity httpSecurity) {
-        httpSecurity.authorizeRequests().antMatchers()
+    protected void configure(HttpSecurity http) throws Exception{
+        http.csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/", "/index", "/about").permitAll()
+                .antMatchers("/admin/**").hasAnyRole("ADMIN")
+                .antMatchers("/user/**").hasAnyRole("USER")
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .permitAll()
+                .and()
+                .logout()
+                .permitAll()
+                .and()
+                .exceptionHandling().accessDeniedHandler(accessDeniedHandler);
+
+
+        //TODO доделать форму логина и логаута
+//        http.formLogin()
+//                // указываем страницу с формой логина
+//                .loginPage("/login")
+//                // указываем action с формы логина
+//                .loginProcessingUrl("/j_spring_security_check")
+//                // указываем URL при неудачном логине
+//                .failureUrl("/login?error")
+//                // Указываем параметры логина и пароля с формы логина
+//                .usernameParameter("j_username")
+//                .passwordParameter("j_password")
+//                // даем доступ к форме логина всем
+//                .permitAll();
+//
+//        http.logout()
+//                // разрешаем делать логаут всем
+//                .permitAll()
+//                // указываем URL логаута
+//                .logoutUrl("/logout")
+//                // указываем URL при удачном логауте
+//                .logoutSuccessUrl("/login?logout")
+//                // делаем не валидной текущую сессию
+//                .invalidateHttpSession(true);
+
+//        httpSecurity.authorizeRequests().antMatchers()
     }
+
+   //Второй вариант configure
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//
+//        http.exceptionHandling().accessDeniedHandler(new AccessDeniedHandlerImpl() {
+//            @Override
+//            public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException{
+//                super.handle(request, response, accessDeniedException);
+//                accessDeniedException.printStackTrace();
+//            }
+//        });
+//    }
 
     @Override
     public AuthenticationManager authenticationManagerBean() throws Exception {
