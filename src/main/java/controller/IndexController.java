5684df542813917d66a87ce78ff1b8143179402a
Montego <bar_d91@mail.ru>
2018-09-12 21:32:40 +0300

    frontend test

---
+++ src/main/java/controller/IndexController.java
@@ -0,0 +1,15 @@
+//package controller;
+//
+//import org.springframework.http.HttpStatus;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.ui.Model;
+//import org.springframework.web.bind.annotation.*;
+//
+//@RestController
+//@RequestMapping("/")
+//public class IndexController {
+//    public String getIndexPage() {
+//        return "index";
+//    }
+//}
--- src/main/java/security/SecurityConfig.java
+++ src/main/java/security/SecurityConfig.java
@@ -43,30 +43,28 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 
         //TODO доделать форму логина и логаута
-//        http.formLogin()
-//                // указываем страницу с формой логина
-//                .loginPage("/login")
-//                // указываем action с формы логина
-//                .loginProcessingUrl("/j_spring_security_check")
-//                // указываем URL при неудачном логине
-//                .failureUrl("/login?error")
-//                // Указываем параметры логина и пароля с формы логина
-//                .usernameParameter("j_username")
-//                .passwordParameter("j_password")
-//                // даем доступ к форме логина всем
-//                .permitAll();
-//
-//        http.logout()
-//                // разрешаем делать логаут всем
-//                .permitAll()
-//                // указываем URL логаута
-//                .logoutUrl("/logout")
-//                // указываем URL при удачном логауте
-//                .logoutSuccessUrl("/login?logout")
-//                // делаем не валидной текущую сессию
-//                .invalidateHttpSession(true);
+        http.formLogin()
+                // указываем страницу с формой логина
+                .loginPage("/login")
+                // указываем action с формы логина
+                .loginProcessingUrl("/j_spring_security_check")
+                // указываем URL при неудачном логине
+                .failureUrl("/login?error")
+                // Указываем параметры логина и пароля с формы логина
+                .usernameParameter("j_username")
+                .passwordParameter("j_password")
+                // даем доступ к форме логина всем
+                .permitAll();
 
-//        httpSecurity.authorizeRequests().antMatchers()
+        http.logout()
+                // разрешаем делать логаут всем
+                .permitAll()
+                // указываем URL логаута
+                .logoutUrl("/logout")
+                // указываем URL при удачном логауте
+                .logoutSuccessUrl("/login?logout")
+                // делаем не валидной текущую сессию
+                .invalidateHttpSession(true);
     }
 
     //Второй вариант configure
