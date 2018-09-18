45c1a76863f99026ed8c3dd4fe8d639c57cce371
Montego <bar_d91@mail.ru>
2018-09-18 20:53:43 +0300

    c

--- src/main/java/repository/ArmorRepository.java
+++ src/main/java/repository/ArmorRepository.java
@@ -1,12 +1,12 @@
 package repository;
 
 import entity.Armor;
-import entity.TypeArmor;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-public interface ArmorRepository extends JpaRepository<Armor, Long>{
+public interface ArmorRepository extends JpaRepository<Armor, Long> {
     Armor findByName(String name);
+
     List<Armor> findByTypeArmor(Enum typeArmor);
 }
